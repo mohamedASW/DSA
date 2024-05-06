7321f6a46ce38dc5d23fa8bf8ec28b0e602863b3
mohamed Ahmed <mohamedAhmed@gmail.com>
2024-05-06 15:10:45 +0300

    intial commit

---
+++ AlgoExtentions.cs
@@ -0,0 +1,257 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Net.Http.Headers;
+using System.Reflection;
+
+namespace AlgoLibrary
+{
+    public static class AlgoExtentions
+    {
+        public static void LinearSearch(this IEnumerable<int> source, int value)
+        {
+            var copy = source.ToArray();
+            int tries = 1;
+            for(int i = 0; i < copy.Length; ++i)
+            {
+                ++tries;
+                if (copy[i] == value)
+                {
+                    Console.WriteLine(@$"it has been founded..! after : {{{tries}}} Tries.");
+                    return;
+                }
+            }
+
+            Console.WriteLine("it has not been founded..!");
+        }
+
+        public static void BinarySearch(this IEnumerable<int> source ,int value)
+        {
+            var copy = source.ToArray();
+
+            int tries = 1;
+            BinarySearchHandler(copy,value,0,copy.Length-1);
+
+            void BinarySearchHandler(int[] arr , int val, int lft , int rht)
+            {
+
+                int mid = lft + (rht - lft) / 2;
+                if (lft<=rht)
+                {
+                    if (val == arr[mid])
+                    {
+                        Console.WriteLine(@$"it has been founded..! ,  after : {{{tries}}} Tries.");
+                        return;
+                    }
+                    else if (val > arr[mid])
+                    {
+
+                        ++tries;
+                        BinarySearchHandler(arr, val,mid + 1, rht);
+                    }
+                    else if (val < arr[mid])
+                    {
+                        ++tries;
+                        BinarySearchHandler(arr, val, lft, mid-1);
+                    }
+
+                }
+                else
+                {
+                    Console.WriteLine("it has not been founded..!");
+                }
+
+            }
+
+        }
+
+        public static void InterPolation(this IEnumerable<int> source, int value)
+        {
+            var copy = source.ToArray();
+
+            int tries = 1;
+            BinarySearchHandler(copy, value, 0, copy.Length - 1);
+
+            void BinarySearchHandler(int[] arr, int val, int lft, int rht)
+            {
+
+                int mid = lft + ((rht - lft) / (arr[rht] - arr[lft])) * val - arr[lft];
+                if (lft <= rht)
+                {
+                    if (val == arr[mid])
+                    {
+                        Console.WriteLine(@$"it has been founded..! ,  after : {{{tries}}} Tries.");
+                        return;
+                    }
+                    else if (val > arr[mid])
+                    {
+
+                        ++tries;
+                        BinarySearchHandler(arr, val, mid + 1, rht);
+                    }
+                    else if (val < arr[mid])
+                    {
+                        ++tries;
+                        BinarySearchHandler(arr, val, lft, mid - 1);
+                    }
+
+                }
+                else
+                {
+                    Console.WriteLine("it has not been founded..!");
+                }
+
+            }
+
+        }
+
+        public static IEnumerable<int> BubbleSort(this IEnumerable<int> source)
+        {
+
+            var copy = source.ToArray();
+
+            for (int i = 1; i < copy.Length; i++)
+            {
+               
+                for (int j = 0; j < copy.Length-i; j++)
+                {
+                    if (copy[j] > copy[j+1])
+                    {
+                        Swap(ref copy[j],ref copy[j + 1]);
+                    }
+                }
+            }
+            return copy;
+        }
+        private  static void Swap(ref int number1 , ref int number2)
+        {
+            int temp= number1;
+            number1 = number2;
+            number2 = temp;
+        }
+      
+        public static IEnumerable<int> MergeSort(this IEnumerable<int> source)
+        {
+
+            var copy = source.ToArray();
+
+            divide(copy, 0, copy.Length-1);
+            // divde 
+
+            void divide(int[] arr , int low , int high)
+            {
+                if (low<high)
+                {
+                int mid = low + (high - low) / 2;
+                divide(arr, low, mid);
+                divide(arr, mid + 1, high);
+                merge(arr, low, mid , high);
+
+                }
+            }
+            //merge
+
+            void merge(int[] arr , int low , int m,int high)
+            {
+                int n1 = m - low +1 ;
+                int n2 = high - m;
+
+                int[] arr1 = new int[n1];
+                int[] arr2 = new int[n2];
+
+                int i = 0;
+                int j = 0;
+                int k;
+                while (i<arr1.Length)
+                {
+                    arr1[i] = arr[i + low];
+                    i++;
+                
+                }
+                while (j < arr2.Length)
+                {
+                    arr2[j] = arr[j +m+1];
+                    j++;
+
+                }
+                i = 0;
+                j = 0;
+                k = low;
+                while(i<arr1.Length && j < arr2.Length)
+                {
+                    if (arr1[i] < arr2[j])
+                    {
+                        arr[k] = arr1[i];
+                        ++i;
+                    }
+                    else
+                    {
+                        arr[k] = arr2[j];
+                        ++j;
+                    }
+                    ++k;
+                }
+                while (i < arr1.Length)
+                {
+                    arr[k] = arr1[i];
+                    ++i;
+                    ++k;
+                }
+                while (j < arr2.Length)
+                {
+                    arr[k] = arr2[j];
+                    ++j;
+                    ++k;
+                }
+            }
+            return copy;
+
+        }
+
+        public static IEnumerable<int>QuickSort(this IEnumerable<int> source)
+        {
+
+            var copy = source.ToArray();
+
+            divide(copy, 0, copy.Length - 1);
+     
+            // divide
+            
+            void divide(int[] arr , int low , int high)
+            {
+                if (low<high)
+                {
+                    int pivotIndex = partition(arr, low, high);
+
+                    divide(arr, pivotIndex + 1, high);
+                    divide(arr , low, pivotIndex-1);
+                }
+            }
+
+
+            // partitioning
+
+            int partition(int[]arr , int low , int high)    // { 12, 4, 5, 6, 7, 3, 1, 15 };
+            {
+                var pivot = arr[high];
+                var i = low - 1; 
+                
+                for (int j= low; j < high; j++)
+                {
+                    if (arr[j] <= pivot)
+                    {
+                        i++;
+                        Swap(ref arr[i], ref arr[j]);
+                    }
+
+                }
+
+                Swap(ref arr[i + 1], ref arr[high]);
+                return i + 1;
+            }
+
+            return copy;
+
+        }
+
+
+    }
+}
---
+++ Graph.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoLibrary
+{
+    public class Graph
+    {
+        int verticesCount;
+        List<int>[] AdjList;
+
+        public Graph(int verticescount)
+        {
+            this.verticesCount = verticescount;
+
+            AdjList = new List<int>[verticescount];
+            for (int i = 0; i < verticesCount; i++)
+            {
+                AdjList[i] = new List<int>();
+            }
+        }
+
+        public void Add(int headvertix , int lastvertix)
+        {
+            AdjList[headvertix].Add(lastvertix);
+        }
+
+
+        public void BFS(int startvertix)
+        {
+            bool[] visited = new bool[verticesCount];
+            Queue<int>vertices = new Queue<int>();
+            visited[startvertix] = true;
+            vertices.Enqueue(startvertix);
+
+            while (vertices.Count!=0)
+            {
+                var n = vertices.Dequeue();
+                Console.WriteLine($"{n}  ");
+
+                foreach (var item in AdjList[n])
+                {
+                    if (!visited[item])
+                    {
+                        visited[item] = true;
+                        vertices.Enqueue(item);
+                    }
+                }
+
+            }
+        } 
+
+        public int ShortestStepsByBFS(int startvertix , int endvertix)
+        {
+            bool[] visited = new bool[verticesCount];
+            int steps  = 0; 
+            bool found = false;
+            Queue<int> vertices = new Queue<int>();
+            visited[startvertix] = true;
+            vertices.Enqueue(startvertix);
+            while (vertices.Count!=0)
+            {
+                var curvertix = vertices.Dequeue();
+                steps++;
+                foreach (var item in AdjList[curvertix])
+                {
+                    if (!visited[item])
+                    {
+                        visited[item] = true;
+                        vertices.Enqueue(item);
+                        
+                        if (item ==endvertix)
+                        {
+                            found = true;
+                            break; 
+                        }
+                    }
+                }
+                if (found)
+                    break;
+            }
+            if (found)
+                return steps; 
+          return -1;
+        }
+
+        public List<int> ShortestPathBFS(int sourcevertix, int endvertix)
+        {
+            bool[] visit = new bool[verticesCount];
+            int[] parents = new int[verticesCount];
+            Array.Fill(parents, -1);
+            Queue<int> vertices = new Queue<int>();
+            visit[sourcevertix] = true;
+            vertices.Enqueue(sourcevertix);
+
+            while (vertices.Count != 0)
+            {
+                var curvertix = vertices.Dequeue();
+                if (curvertix == endvertix)
+                    break;
+                foreach (var neighbor in AdjList[curvertix])
+                {
+                    if (!visit[neighbor])
+                    {
+                        visit[neighbor] = true;
+                        parents[neighbor] = curvertix;
+                        vertices.Enqueue(neighbor);
+                    }
+                }
+
+            }
+
+            List<int> bactrackthepath = new List<int>();
+            int vertix = endvertix;
+            while (vertix != -1)
+            {
+                bactrackthepath.Add(vertix);
+                vertix = parents[vertix];
+            }
+            bactrackthepath.Reverse();
+            return bactrackthepath;
+
+        }
+
+        public  void DFS(int startvertix)
+        {
+            bool[] visited  = new bool[verticesCount];
+           DFSUtlization(startvertix, visited);
+        }
+
+        private void DFSUtlization(int startvertix, bool[] visited)
+        {
+            
+            
+                Console.Write($"{startvertix}  ");
+                visited[startvertix] = true;
+                foreach (var vertix in AdjList[startvertix])
+                {
+                    if (!visited[vertix])
+                        DFSUtlization(vertix, visited);
+                }
+
+            
+        }
+
+        private List<int> DFSshortestpath(int v , int target , bool[] visited , List<int> path , List<List <int>> paths)
+        {
+
+            visited[v] = true;
+            path.Add(v);
+            if (v == target)
+            {
+                paths.Add(new List<int>(path));
+               
+            }
+            else
+            {
+
+                foreach (int vertix in AdjList[v])
+                {
+                    if (!visited[vertix])
+                    {
+                        DFSshortestpath(vertix, target, visited, path, paths);
+                        
+                    }
+                }
+            }
+
+            path.RemoveAt(path.Count - 1);
+            visited[v] = false;
+            return path;
+
+        }
+
+        public List<int>? FindPath(int src, int target)
+        {
+            bool[] visited = new bool[verticesCount];
+            List<int> path = new List<int>();
+            List<List<int>> paths = new List<List<int>>();
+            DFSshortestpath(src, target, visited, path, paths);
+            return paths.MinBy(p => p.Count());
+        }
+    }
+}
